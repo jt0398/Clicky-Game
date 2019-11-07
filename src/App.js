1d82a5f1a135592fafa725a33a728bf7e78a051f
Judy <judys717@hotmail.com>
2019-11-07 01:09:02 -0500

    Add comments

--- src/App.js
+++ src/App.js
@@ -6,28 +6,39 @@ import characters from "./lotr.json";
 
 class App extends React.Component {
   state = {
-    score: 0,
-    topScore: 0,
-    message: "",
-    sortedCharacters: []
+    score: 0, //User's count of correct guesses
+    topScore: 0, //User's highest score
+    message: "", //Displays messages incorrect or correct depending on user's action
+    sortedCharacters: [] //An array of character objects. The order is random.
   };
 
   handleCharClick = id => {
+    //Checks if the character has been selected before
     if (characters[id - 1].clicked) {
+      /*If the character has been selected before
+      and the current score is highest score,
+      update the highest score value
+      */
       let topScore = this.state.topScore;
 
       if (this.state.score > topScore) {
         topScore = this.state.score;
       }
 
+      //Resets score and displays "You guess incorrectly"
       this.setState({ message: "incorrectly", score: 0, topScore: topScore });
     } else {
+      /* If character has not been selected before,
+        update character's object clicked property value to true.
+        Then increase the score incrementally and displays messaage "You guessed correctly"
+      */
       characters[id - 1].clicked = true;
       this.setState((prevState, props) => {
         return { score: prevState.score + 1, message: "correctly" };
       });
     }
 
+    //Render the character images
     this.loadCharacters();
   };
 
@@ -35,19 +46,27 @@ class App extends React.Component {
     let ids = [];
     let characterList = [];
 
+    //Numbers 0 to 11 is added to the array
     for (let i = 0; i < 12; i++) {
+      //If the user guess incorrectly, resets all the character clicked property to false
       if (this.state.score === 0) {
         characters[i].clicked = false;
       }
       ids.push(i);
     }
 
+    /* While there's an id in the array, 
+    generate a random number 11 times. The random number
+    is equivalent to the index or position of character
+    in the original character array. The character is then
+    added to the sorted array */
     while (ids.length > 0) {
       const randomNum = Math.floor(Math.random() * ids.length);
       const id = ids.splice(randomNum, 1);
       characterList.push(characters[id]);
     }
 
+    //Render the character images. The order of the images is random
     this.setState({ sortedCharacters: characterList });
   };
 
@@ -58,12 +77,14 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        {/*NAV*/}
         <Nav
           message={this.state.message}
           score={this.state.score}
           topScore={this.state.topScore}
         />
         <Container>
+          {/*Character Images*/}
           <CardColumns>
             {this.state.sortedCharacters.map(character => {
               return (
