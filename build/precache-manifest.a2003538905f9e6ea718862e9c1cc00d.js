dadbac65b2d083f2865e9e13bff1accd3dc05261
Judy <judys717@hotmail.com>
2019-11-07 01:13:30 -0500

    React build

---
+++ build/precache-manifest.a2003538905f9e6ea718862e9c1cc00d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04966d25126c386e66903d35cf73a1bf",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "52a8569a6a318e326c9e",
+    "url": "/clicky-game/static/css/main.b4711682.chunk.css"
+  },
+  {
+    "revision": "8ddf23b8ae8e7f5c8847",
+    "url": "/clicky-game/static/js/2.19c06209.chunk.js"
+  },
+  {
+    "revision": "52a8569a6a318e326c9e",
+    "url": "/clicky-game/static/js/main.90914d36.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
